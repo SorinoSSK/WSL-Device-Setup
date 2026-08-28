4d5078b76f056ecb875255de8b478692962b1dee
sorinosk <sweeapplemage@hotmail.com>
2026-08-28 11:09:06 +0800

    Updated missing path in bashrc for claude

--- libraries/setup-claude-agent.sh
+++ libraries/setup-claude-agent.sh
@@ -8,4 +8,11 @@ _install_claude()
 
     local script_dir="${1:-}"
     cp -R "${script_dir}/${AGENTS_DIR}" ~/.claude/agents
+
+    if ! grep -q "\${HOME}/.local/bin:\${PATH}" "${HOME}/.bashrc"; then
+        echo 'export PATH="${HOME}/.local/bin:${PATH}"' >> ~/.bashrc
+        _print_info "Added claude path into bash."
+    else
+        _print_info "Claude path is already in bash."
+    fi
 }
